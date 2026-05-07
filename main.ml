7f40caf8ce8c2d4f93078963a1b8cdbcda9da407
tom <81908260+ungleicch@users.noreply.github.com>
2026-05-07 13:08:14 -0700

    raw structure

---
+++ main.ml
@@ -0,0 +1,40 @@
+
+
+let mat_zeros = (**)
+let mat_id = (**)
+let mat_copy = (**)
+
+let mat_add = (**)
+
+let mat_mul = (**)
+
+let mat_vec = (**)
+
+let vec_add = (**)
+let vec_scale = (**)
+let vec_dot = (**)
+
+let argmax = (**)
+
+let mat_exp = (**)
+
+let randn = (**)
+
+
+type ssm = (**)
+type linear = (**)
+
+let make_ssm  = (**)
+
+let discretize = (**)
+
+let ssm_fwd = (**)
+
+let ssm_bwd = (**)
+
+let softmax = (**)
+
+
+type adam = (**)
+
+let new_adam = (**)
