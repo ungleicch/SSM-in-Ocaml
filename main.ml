e0628ee597703e9be7bc9d4cca9ad994af8dc4df
tom <81908260+ungleicch@users.noreply.github.com>
2026-05-07 13:19:22 -0700

    adding simle matrix operations I.

--- main.ml
+++ main.ml
@@ -1,10 +1,17 @@
 
+(* create a matrix with all zero*)
+let mat_zeros r c = Array.init r (fun _ -> Array.make c 0.0)
 
-let mat_zeros = (**)
-let mat_id = (**)
-let mat_copy = (**)
+(* create identity matrix*)
+let mat_id n = Array.init n (fun i -> Array.init n (fun j -> if i = j then 1, else 0.))
 
-let mat_add = (**)
+(* create an independent copy of the matrix*)
+let mat_copy m = Array.map Array.copy m
+
+(* element wise adding of two matrices*)
+let mat_add a b =
+  let r = Array.length a and c = Array.length a.(0) in
+  Array.init r (fun i -> Array.init c (fun j -> a.(i.(j) +. b.(i).(j)))
 
 let mat_mul = (**)
 
